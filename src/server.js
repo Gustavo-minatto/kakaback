bc036ff0bbacccfa378249df9d2f3188d65dd499
Gustavo Minatto <gustavominatto23@gmail.com>
2024-12-11 16:12:57 -0300

    tentando subir pra produção

--- src/server.js
+++ src/server.js
@@ -1,19 +1,39 @@
+require("express-async-errors");
+require("dotenv/config");
+
+const uploadConfig = require("./configs/upload");
+
+const migrationsRun = require("./database/sqlite/migrations");
+
 const express = require("express");
-const cors = require("cors"); 
 const routes = require("./routes");
+const cors = require("cors");
+
+migrationsRun();
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
-app.use(cors({
-  origin: "http://localhost:5173",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"] 
-}));
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
 
-app.use(express.json()); 
-app.use(routes); // Suas rotas
+app.use(routes);
 
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+app.use((error, request, response, next) => {
+  if (error) {
+    return response.status(error.statusCode).json({
+      status: "error",
+      message: error.message
+    });
+  }
+
+  console.error(error);
+
+  return response.status(500).json({
+    status: "error",
+    message: "Internal server error",
+  })
 });
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
