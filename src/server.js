ed581fe00e35368939051f28101dff746f174b39
Gustavo Minatto <gustavominatto23@gmail.com>
2024-12-11 16:46:48 -0300

    tentando subir pra produção

--- src/server.js
+++ src/server.js
@@ -3,15 +3,8 @@ require("dotenv/config");
 
 const express = require("express");
 const routes = require("./routes");
-const cors = require("cors");
 
 const app = express();
-
-app.use(cors({
-  origin: 'http://localhost:5173',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
 
 app.use(express.json());
 app.use(routes);
