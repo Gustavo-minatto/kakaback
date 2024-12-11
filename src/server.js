c3631b3955095187389047d75e4eef8fa1a522be
Gustavo Minatto <gustavominatto23@gmail.com>
2024-12-11 16:56:08 -0300

    cors

--- src/server.js
+++ src/server.js
@@ -2,9 +2,17 @@ require("express-async-errors");
 require("dotenv/config");
 
 const express = require("express");
+const cors = require("cors");
 const routes = require("./routes");
 
 const app = express();
+const corsOptions = {
+  origin: 'https://testeadvocacia.netlify.app', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(routes);
